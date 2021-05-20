7bafde9c6831d50bdda6cb0c239591b0f77f0c27
Hiro Ishida <hiroci260500@gmail.com>
2021-05-20 13:39:49 -0300

    Adicionando bluetooth.h

---
+++ robouvc.X/geral.h
@@ -0,0 +1,67 @@
+/* 
+ * File:   geral.h
+ * Author: Hiro
+ *
+ * Created on 19 de Maio de 2021, 19:36
+ */
+void configBits() {
+    // CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+#pragma config IESO = ON        // Internal External Oscillator Switchover bit (Internal External Switchover mode enabled)
+
+    // CONFIG2L
+#pragma config PWRTEN = OFF     // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled)
+    // BORV = No Setting
+
+    // CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDPS = 32768     // Watchdog Timer Postscale Select bits (1:32768)
+#pragma config WINEN = OFF      // Watchdog Timer Window Enable bit (WDT window disabled)
+
+    // CONFIG3L
+#pragma config PWMPIN = OFF     // PWM output pins Reset state control (PWM outputs disabled upon Reset (default))
+#pragma config LPOL = HIGH      // Low-Side Transistors Polarity (PWM0, 2, 4 and 6 are active-high)
+#pragma config HPOL = HIGH      // High-Side Transistors Polarity (PWM1, 3, 5 and 7 are active-high)
+#pragma config T1OSCMX = ON     // Timer1 Oscillator MUX (Low-power Timer1 operation when microcontroller is in Sleep mode)
+
+    // CONFIG3H
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (Enabled)
+
+    // CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Low-Voltage ICSP Enable bit (Low-voltage ICSP enabled)
+
+    // CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000200-000FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (001000-001FFF) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (002000-002FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (003000-003FFFh) not code-protected)
+
+    // CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot Block (000000-0001FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+    // CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000200-000FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (001000-001FFF) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (002000-002FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (003000-003FFFh) not write-protected)
+
+    // CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0001FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+    // CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000200-000FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (001000-001FFF) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (002000-002FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (003000-003FFFh) not protected from table reads executed in other blocks)
+
+    // CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from table reads executed in other blocks)
+
+}
+
--- robouvc.X/main.c
+++ robouvc.X/main.c
@@ -7,7 +7,42 @@
 
 
 #include <xc.h>
+#include <pic18f4431.h>
+#include "PWM.h"
+
+#define _XTAL_FREQ 20000000
+#define VELOCIDADE_MAXIMA 100
+
+#define SL7 PORTAbits.RA0
+#define SL6 PORTAbits.RA1
+#define SL5 PORTAbits.RA2
+#define SL4 PORTAbits.RA3
+#define SL3 PORTAbits.RA4
+#define SL2 PORTAbits.RA5
+#define SL1 PORTEbits.RE0
+
 
 void main(void) {
+    
+    /*seta todas as portas do PORTA como an
+     * e as portas RE0, RE1 e RE2 como an
+     */
+    ANSEL0 = 0b11111111;
+    ANSEL1 = 0b00000001;
+    
+    TRISA = 0b11111111;//seta todas as portas do PORTA como an input
+    TRISB = 0b00000000;
+    TRISC = 0b00000000;
+    TRISD = 0b00011100;
+    TRISE = 0b00000111;//seta as portas 0,1,2 como an input
+    
+    PORTB = 0b00000000;
+    PORTC = 0b00000000;
+    PORTD = 0b00000000;
+    PORTE = 0b00000000;
+    
+    configBits();
+    configPWM();
+    
     return;
 }
